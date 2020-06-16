803c55f705e0c29b2c3ac2c824f286eb96066a8b
Panos Meretis <panagiotis.meretis@lmtom.london>
2020-06-16 10:01:29 +0100

    fix in order to get the title

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,6 @@
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
-        browser.url('https://https://sand-developerportal-lloydsapi.azurewebsites.net/')
+        browser.url('https://sand-developerportal-lloydsapi.azurewebsites.net/')
         const title = browser.getTitle()
         expect(browser).toHaveTitle("Lloyd's Developer Portal");
     })
